6e35ca954c74355bc8c3137dc01fa8069d793fc1
Tom Carmona <thomascarmona@gmail.com>
2015-05-18 16:47:49 -0500

    move rows with drag

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,12 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UIGestureRecognizerDelegate>
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UIGestureRecognizerDelegate, UIAlertViewDelegate>
 
 @property NSMutableArray *cellArray;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property UITableViewCell *cell, *swipedRightCell;
+@property BOOL delete;
 
 @end
 
@@ -23,6 +24,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.cellArray = [[NSMutableArray alloc] init];
+    self.delete = 0;
 }
 
 - (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
@@ -30,6 +32,7 @@
     if ([sender.title  isEqual: @"Edit"]) {
         sender.title = @"Done";
         [self.tableView setEditing:YES animated:YES];
+        self.editing = YES;
     } else {
         sender.title = @"Edit";
         [self.tableView setEditing:NO animated:YES];
@@ -37,16 +40,51 @@
 }
 - (IBAction)swipedRight:(id)sender {
     NSLog(@"Swipe returns object: %@", sender);
-    self.cell.textLabel.textColor = [UIColor redColor];
+    if ([self.cell.textLabel.textColor isEqual:[UIColor blackColor]]) {
+        self.cell.textLabel.textColor = [UIColor greenColor];
+    } else if ([self.cell.textLabel.textColor isEqual:[UIColor greenColor]]){
+        self.cell.textLabel.textColor = [UIColor yellowColor];
+    } else if ([self.cell.textLabel.textColor isEqual: [UIColor yellowColor]]){
+        self.cell.textLabel.textColor = [UIColor redColor];
+    } else {
+        self.cell.textLabel.textColor = [UIColor blackColor];
+    }
+
+}
+
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
+    NSString *stringMove = self.cellArray[sourceIndexPath.row];
+    [self.cellArray removeObjectAtIndex:sourceIndexPath.row];
+    [self.cellArray insertObject:stringMove atIndex:destinationIndexPath.row];
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Delete" message:@"Are you sure you want to delet this?" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Yes, delete", nil];
+        [alert show];
+
+        if (self.delete == 1) {
         [self.cellArray removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            self.delete = 0;
+        }
+
+
         [self.tableView reloadData];
     } else {
         NSLog(@"Unhandled editing style! %ld", (long)editingStyle);
+    }
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 0) {
+        //pressed cancel, do nothing
+    } else if (buttonIndex == 1) {
+         self.delete = 1;
     }
 }
 
